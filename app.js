02b245a6c492a23ef2d55d66019ce22dc38c898f
Ivan Bogdan <bogdan.ivan@nsoft.com>
2019-10-16 14:36:24 +0200

    Prevedimo aplikaciju u Express radi funkcionalnosti i čitljivosti

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-// OSNOVNI MODULI
-const http = require('http');
-const url = require('url');
-const querystring = require('querystring');
+const express = require('express');
+const app = express();
 
-// THIRD-PARTY MODULI
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
-const server = http.createServer((req, res) => {
-  // PARSIRAMO URL I IZ NJEGA IZVLAČIMO POTREBNE PODATKE
-  const { pathname, query } = url.parse(req.url);
-  const { id } = querystring.parse(query);
+app.get('/', (req, res) => {
+  res.status(200).send('Naslovnica');
+});
 
-  // ODGOVOR ŠALJEMO OVISNO O IZLAZNOJ TOČKI
-  if ( pathname === '/' ) {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end('<h1>Naslovnica</h1>')
-  } else if ( pathname === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json' });
-    res.end({ ime: 'Ivan', prezime: 'Bogdan' });
-  } else {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end('<h1>Naslovnica</h1>')
-  }
+//
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { ime: 'Ivan', prezime: 'Bogdan' }
+  });
+});
+
+// MIDDLEWARE 
+app.use((req, res) => {
+  res.status(404).send('Page not found!');
 });
 
 const PORT = process.env.PORT;
-server.listen(PORT, '127.0.0.1', () => {
-  console.log(`Slušamo na portu ${PORT}!`);
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`Listening on port ${PORT}!`);
 });
