5f02033a667ad92789e714f56145bf8ffa96c0b8
Ivan Bogdan <bogdan.ivan@nsoft.com>
2019-10-30 00:09:02 +0100

    Add console output to evaluate db query time

--- src/api/mongoose/users/users.controller.js
+++ src/api/mongoose/users/users.controller.js
@@ -3,7 +3,9 @@ const User = require('./users.model');
 module.exports = {
   getUsers: async (req, res) => {
     try {
+      console.time('GET /mongodb/users');
       const users = await User.find();
+      console.timeEnd('GET /mongodb/users');
       res.status(200).json(users);
     } catch(err) {
       res.status(404).json({});
@@ -11,7 +13,9 @@ module.exports = {
   },
   getUser: async (req, res) => {
     try {
+      console.time(`GET /mongodb/users/${req.params.id}`);
       const user = await User.findById(req.params.id);
+      console.timeEnd(`GET /mongodb/users/${req.params.id}`);
       res.status(200).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -19,7 +23,9 @@ module.exports = {
   },
   postUser: async (req, res) => {
     try {
+      console.time(`POST /mongodb/users/${req.params.id}`);
       const user = await User.create(req.body);
+      console.timeEnd(`POST /mongodb/users/${req.params.id}`);
       res.status(201).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -27,10 +33,12 @@ module.exports = {
   },
   putUser: async (req, res) => {
     try {
+      console.time(`PUT /mongodb/users/${req.params.id}`);
       const user = await User.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         useValidators: true
       });
+      console.timeEnd(`PUT /mongodb/users/${req.params.id}`);
       res.status(204).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -38,7 +46,9 @@ module.exports = {
   },
   deleteUser: async (req, res) => {
     try {
+      console.time(`DELETE /mongodb/users/${req.params.id}`);
       User.findByIdAndDelete(req.params.id);
+      console.timeEnd(`DELETE /mongodb/users/${req.params.id}`);
       res.status(204).json({});
     } catch(err) {
       res.status(404).json({});
--- src/api/sequelize/users/users.controller.js
+++ src/api/sequelize/users/users.controller.js
@@ -3,7 +3,9 @@ const User = require('./users.model');
 module.exports = {
   getUsers: async (req, res) => {
     try {
+      console.time('GET /mariadb/users');
       const users = await User.findAll();
+      console.timeEnd('GET /mariadb/users');
       res.status(200).json(users);
     } catch(err) {
       res.status(404).json({});
@@ -11,7 +13,9 @@ module.exports = {
   },
   getUser: async (req, res) => {
     try {
+      console.time(`GET /mariadb/users/${req.params.id}`);
       const user = await User.findByPk(req.params.id);
+      console.timeEnd(`GET /mariadb/users/${req.params.id}`);
       res.status(200).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -19,7 +23,9 @@ module.exports = {
   },
   postUser: async (req, res) => {
     try {
+      console.time(`POST /mariadb/users/${req.params.id}`);
       const user = await User.create(req.body);
+      console.timeEnd(`POST /mariadb/users/${req.params.id}`);
       res.status(201).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -27,8 +33,10 @@ module.exports = {
   },
   putUser: async (req, res) => {
     try {
+      console.time(`PUT /mariadb/users/${req.params.id}`);
       const user = await User.findByPk(req.params.id)
-      user.update(req.body);
+      await user.update(req.body);
+      console.timeEnd(`PUT /mariadb/users/${req.params.id}`);
       res.status(204).json(user);
     } catch(err) {
       res.status(404).json({});
@@ -36,9 +44,11 @@ module.exports = {
   },
   deleteUser: async (req, res) => {
     try {
+      console.time(`DELETE /mariadb/users/${req.params.id}`);
       const user = await User.findByPk(req.params.id)
-      user.destroy();
-      res.status(204).json(user);
+      await user.destroy();
+      console.timeEnd(`DELETE /mariadb/users/${req.params.id}`);
+      res.status(204).json({});
     } catch(err) {
       res.status(404).json({});
     }
