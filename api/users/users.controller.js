a0b6301e7948ca9555f13a1c089b2cac53c411bf
Ivan Bogdan <bogdan.ivan@nsoft.com>
2019-10-31 09:19:36 +0100

    Add basic error handling

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -2,26 +2,51 @@ const User = require('./users.model');
 
 module.exports = {
   getUsers: async (req, res) => {
-    const users = await User.find();
-    res.status(200).json(users);
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({});
+    }
   },
   getUser: async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({});
+    }
   },
   postUser: async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
+    try {
+      const user = await User.create(req.body);
+      res.status(201).json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({});
+    }
   },
   putUser: async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      useValidators: true
-    })
-    res.status(204).json(user);
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        useValidators: true
+      })
+      res.status(204).json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({});
+    }
   },
   deleteUser: async (req, res) => {
-    User.findByIdAndDelete(req.params.id);
-    res.status(204).json({});
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(204).json({});
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({});
+    }
   }
 }
